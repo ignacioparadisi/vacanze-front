0a5cbfbd48a53059ad07b31acd3b4fb8d4700ed7
marcotaku95 <marco4soul@gmail.com>
2019-04-29 23:14:01 -0400

    fix:module.ts flight-reservation

--- src/app/components/flight-reservations/flight-reservations.module.ts
+++ src/app/components/flight-reservations/flight-reservations.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FlightReservationsComponent} from './flight-reservations.component';
+import {FlightReservationsRoutingModule} from './flight-reservations-routing.module';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     FlightReservationsComponent],
   imports: [
+    FlightReservationsRoutingModule,
     CommonModule,
     HttpClientModule,
     NgbModule
