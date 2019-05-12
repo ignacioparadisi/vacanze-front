c2ab5ffc352ce8c4419393ee29b5dc614b6f5a0c
marcotaku95 <marco4soul@gmail.com>
2019-05-12 11:08:45 -0400

    fix:arreglar sintaxis

--- src/app/classes/people_flight.ts
+++ src/app/classes/people_flight.ts
@@ -1,13 +1,23 @@
-export class people_flight {
-    id: number;
-    name: string;
+export class People_flight {
+    private _id: number;
+    private _name: string;
     
-  
     constructor(id: number, name: string) {
       this.id = id;
       this.name = name;
     }
-
+    public get id(): number {
+      return this._id;
+    }
+    public set id(value: number) {
+      this._id = value;
+    }
+    public get name(): string {
+      return this._name;
+    }
+    public set name(value: string) {
+      this._name = value;
+    }
     
   }
   
--- src/app/components/flight-reservations/flight-reservations.component.ts
+++ src/app/components/flight-reservations/flight-reservations.component.ts
@@ -1,11 +1,11 @@
 import { ApiService } from 'src/app/services/api.service';
 import { Component, OnInit, Input } from '@angular/core';
-import { class_flight } from 'src/app/classes/class_flight';
-import { type_flight } from 'src/app/classes/type_flight';
+import { Class_flight } from 'src/app/classes/class_flight';
+import { Type_flight } from 'src/app/classes/type_flight';
 import Swal from 'sweetalert2';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
-import { people_flight } from 'src/app/classes/people_flight';
+import { People_flight } from 'src/app/classes/people_flight';
 @Component({
   selector: 'app-flight-reservations',
   templateUrl: './flight-reservations.component.html',
@@ -14,15 +14,13 @@ import { people_flight } from 'src/app/classes/people_flight';
 export class FlightReservationsComponent implements OnInit {
 
   public form: FormGroup;
-  public class_flight: class_flight[] = [];
-  public type_flight: type_flight[]=[];
-  public kids_flight:people_flight[]=[];
-  public adult_flight:people_flight[]=[];
+  public class_flight: Class_flight[] = [];
+  public type_flight: Type_flight[]=[];
+  public adult_flight:People_flight[]=[];
 
 
   constructor(private api: ApiService) { }
-   isDisabled: boolean = true;
-   selectedTyp: string="";
+   private selectedTyp: string="";
    
   ngOnInit() {
       this.fetchFlight();
@@ -41,40 +39,40 @@ export class FlightReservationsComponent implements OnInit {
 
   }
 
-  private fetchFlight(): class_flight[] {
+  private fetchFlight(): Class_flight[] {
     this.class_flight = [
-      new class_flight(0, 'Economy'),
-      new class_flight(1, 'Premium Economy'),
-      new class_flight(2, 'Ejecutiva'),
-      new class_flight(3, 'Primera Clase'),
+      new Class_flight(0, 'Economy'),
+      new Class_flight(1, 'Premium Economy'),
+      new Class_flight(2, 'Ejecutiva'),
+      new Class_flight(3, 'Primera Clase'),
     ];
     return this.class_flight;
   }
 
-  private typeFlight(): type_flight[]{
+  private typeFlight(): Type_flight[]{
     this.type_flight=[
-      new type_flight(0,'Ida'),
-      new type_flight(1,'Ida y Vuelta '),
-      new type_flight(2,'Multidestinos'), 
+      new Type_flight(0,'Ida'),
+      new Type_flight(1,'Ida y Vuelta '),
+      new Type_flight(2,'Multidestinos'), 
     ];
     return this.type_flight; 
   }
 
  
-  private adultFlight():people_flight[]{
+  private adultFlight():People_flight[]{
 
    
       this.adult_flight=[
-        new people_flight(1,'a'),
-        new people_flight(2,'a'),
-        new people_flight(3,'a'),
-        new people_flight(4,'a'),
-        new people_flight(5,'a'),
-        new people_flight(6,'a'),
-        new people_flight(7,'a'),
-        new people_flight(8,'a'),
-        new people_flight(9,'a'),
-        new people_flight(10,'a'),
+        new People_flight(1,'a'),
+        new People_flight(2,'a'),
+        new People_flight(3,'a'),
+        new People_flight(4,'a'),
+        new People_flight(5,'a'),
+        new People_flight(6,'a'),
+        new People_flight(7,'a'),
+        new People_flight(8,'a'),
+        new People_flight(9,'a'),
+        new People_flight(10,'a'),
       ];
    
     return this.adult_flight;
@@ -83,7 +81,6 @@ export class FlightReservationsComponent implements OnInit {
     this.selectedTyp = event.target.id ;
     var input= document.getElementById("entry_id");
     var output= document.getElementById("out_id");
-
     var element =<HTMLInputElement> document.getElementById("date_id");
     element.checked = null;
     if (this.selectedTyp=="0") {
