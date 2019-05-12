95dfc37bb821db2089db4637d3411345fb1b3f8d
Javier Stifano <jstifano18@gmail.com>
2019-05-12 01:40:41 -0400

    Añadiendo variable endpoint al objeto de ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+import * as api_endpoints from './api_endpoints.json';
 export const environment = {
   production: false,
-  baseApiUrl: "https://localhost:5001"
+  baseApiUrl: "https://localhost:5001",
+  endpoint: api_endpoints
 };
 
 /*
