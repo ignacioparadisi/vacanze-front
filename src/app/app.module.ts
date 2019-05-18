2afbf608ea5e71e1b98fa0ade10a7b5a316156fb
rafael sanchez <rafaelsanchez814@gmail.com>
2019-05-18 09:34:09 -0400

    Create Modulo de pago
    se creo componente de y modulo de pago 
    llamado grupo-once-pago

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from "./app-routing.module";
+import { GrupoOncePagoComponent } from './components/grupo-once-pago/grupo-once-pago.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent  ],
+    AppComponent,
+    GrupoOncePagoComponent  ],
     imports: [
     BrowserModule,
     AppRoutingModule
---
+++ src/app/components/grupo-once-pago/grupo-once-pago.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GrupoOncePagoRoutingModule } from './grupo-once-pago-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GrupoOncePagoRoutingModule
+  ]
+})
+export class GrupoOncePagoModule { }
