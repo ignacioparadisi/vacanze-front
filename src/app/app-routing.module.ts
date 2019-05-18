c89bb36015f219851719041a68a7c49ec39a0d48
Kevin Nava <hombrehealth@hotmail.com>
2019-05-18 16:13:26 -0500

    login bulding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
 import {LoginComponent} from "./components/grupo-uno/login/login.component";
-
-
 // El componente principal carga de forma Lazy el Layout.
 const routes: Routes = [
-  { path: "", loadChildren: "./layout/layout.module#LayoutModule" },
+  //{ path: "", loadChildren: "./layout/layout.module#LayoutModule" },
+  {path:"",redirectTo:'/login',pathMatch:'full'},
   { path: "login", component: LoginComponent}
 ];
 
