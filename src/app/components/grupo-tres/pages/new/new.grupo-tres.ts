74b518eb66adf3f78720115fb20f1adec2216913
Alexander <alexander_deazevedo@hotmail.com>
2019-05-19 21:46:57 -0400

    enviando formulario con formarray

--- src/app/components/grupo-tres/pages/new/new.grupo-tres.ts
+++ src/app/components/grupo-tres/pages/new/new.grupo-tres.ts
@@ -15,24 +15,57 @@ export class NewGrupoTres implements OnInit {
     public countrys = COUNTRYS;
     public hours = HOURS;
     public minutes = MINUTES;
-    form: FormGroup;
+    public form: FormGroup;
+    public contactList: FormArray;
 
     constructor(private modalService: NgbModal, private fb: FormBuilder) {
-        this.form = this.fb.group({
-            published: true,
-            credentials: this.fb.array([]),
-        });
     }
 
     ngOnInit() {
+        this.form = this.fb.group({
+            contacts: this.fb.array([this.createContact()])
+        });
+
+        // set contactlist to the form control containing contacts
+        this.contactList = this.form.get('contacts') as FormArray;
     }
 
-    addCreds() {
-    const creds = this.form.controls.credentials as FormArray;
-    creds.push(this.fb.group({
-        username: '',
-        password: '',
-    }));
+    get contactFormGroup() {
+    return this.form.get('contacts') as FormArray;
+    }
+
+    createContact(): FormGroup {
+    return this.fb.group({
+        countryOrigen: [null, Validators.compose([Validators.required])],
+        cityOrigen: [null, Validators.compose([Validators.required])],
+        countryLlegada: [null, Validators.compose([Validators.required])],
+        cityLlegada: [null, Validators.compose([Validators.required])],
+        airplane: [null, Validators.compose([Validators.required])],
+        price: [null, Validators.compose([Validators.required])],
+        dateSalida: [null, Validators.compose([Validators.required])],
+        dateLlegada: [null, Validators.compose([Validators.required])],
+        durationHours: [null, Validators.compose([Validators.required])],
+        durationMinutes: [null, Validators.compose([Validators.required])],
+    });
+    }
+
+    addContact() {
+    this.contactList.push(this.createContact());
+    }
+
+    removeContact(index) {
+    this.contactList.removeAt(index);
+    }
+
+    // get the formgroup under contacts form array
+    getContactsFormGroup(index): FormGroup {
+        // this.contactList = this.form.get('contacts') as FormArray;
+        const formGroup = this.contactList.controls[index] as FormGroup;
+        return formGroup;
+    }
+
+    submit() {
+        console.log(this.form.value);
     }
 
     open(content) {
