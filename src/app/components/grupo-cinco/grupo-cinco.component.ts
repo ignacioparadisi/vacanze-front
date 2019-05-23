08d7d8a706799ed0ddc12ff94a48251cd1629d00
Yisusrly <45052243+Yisusrly@users.noreply.github.com>
2019-05-22 21:03:16 -0400

    version 1 de nuestra ventana.

--- src/app/components/grupo-cinco/grupo-cinco.component.ts
+++ src/app/components/grupo-cinco/grupo-cinco.component.ts
@@ -2,16 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
-
 @Component({
   selector: 'app-grupo-cinco',
   templateUrl: './grupo-cinco.component.html',
   styleUrls: ['./grupo-cinco.component.scss']
 })
 export class GrupoCincoComponent implements OnInit {
-  constructor() { }
+  
+  closeResult: string;
+  constructor(private modalService: NgbModal) { }
+
+  open(content) {
+      this.modalService.open(content).result.then((result) => {
+          this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
+  }
+
+  private getDismissReason(reason: any): string {
+      if (reason === ModalDismissReasons.ESC) {
+          return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+          return 'by clicking on a backdrop';
+      } else {
+          return  `with: ${reason}`;
+      }
+  }
 
   ngOnInit() {
+  }
+
+  deleteFile(){
+      console.log("Registro eliminado")
   }
 
 }
