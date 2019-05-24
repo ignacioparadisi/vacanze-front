f174addd0dbc26df5901af67a69666267271914e
cesarbonadio <cesarbonadio123@gmail.com>
2019-05-23 22:52:39 -0400

    feat: agregar validaciones a los campos del formulario de creacion de hoteles

--- src/app/components/view-hotels-backoffice/register-hotel/register-hotel.component.ts
+++ src/app/components/view-hotels-backoffice/register-hotel/register-hotel.component.ts
@@ -12,9 +12,54 @@ import { FormGroup, FormControl, Validators, FormsModule } from '@angular/forms'
 })
 export class RegisterHotelComponent implements OnInit {
 
-  constructor(private _location: Location) { }
+
+  public registrationForm : FormGroup = new FormGroup({
+    nombre : new FormControl('',[
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(100)
+    ]),
+    capacidad : new FormControl('',[
+      Validators.required,
+      Validators.min(1)
+    ]),
+    cantidadHabitaciones : new FormControl('',[
+      Validators.required,
+      Validators.min(1)
+    ]),
+    telefono : new FormControl('',[
+      Validators.pattern("/([0-9\s\-]{7,})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/")
+    ]),
+    web : new FormControl('',[
+      Validators.pattern("(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})")
+    ])
+  });
+
+
+
+  constructor(private _location: Location){}
 
   ngOnInit() {
+  }
+
+  get nombre(){
+    return this.registrationForm.get('nombre');
+  }
+
+  get telefono(){
+    return this.registrationForm.get('telefono');
+  }
+
+  get capacidad(){
+    return this.registrationForm.get('capacidad');
+  }
+
+  get cantidadHabitaciones(){
+    return this.registrationForm.get('cantidadHabitaciones');
+  }
+
+  get web(){
+    return this.registrationForm.get('web');
   }
 
   public goToViewHotels(){
