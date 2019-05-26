9837fb0b4e192ad1490373a27d117c672a1928f0
cesarbonadio <cesarbonadio123@gmail.com>
2019-05-26 16:44:41 -0400

    feat: agregado formulario de edicion de hotel

--- src/app/components/view-hotels-backoffice/edit-hotel/edit-hotel.component.ts
+++ src/app/components/view-hotels-backoffice/edit-hotel/edit-hotel.component.ts
@@ -1,4 +1,10 @@
+import { ApiService } from 'src/app/services/api.service';
 import { Component, OnInit } from '@angular/core';
+import { Role } from 'src/app/classes/role';
+import { Location } from "@angular/common";
+import Swal from 'sweetalert2';
+import { FormGroup, FormControl, Validators, FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-edit-hotel',
@@ -7,9 +13,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditHotelComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    public registrationForm : FormGroup = new FormGroup({
+      nombre : new FormControl('',[
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(100)
+      ]),
+      capacidad : new FormControl('',[
+        Validators.required,
+        Validators.min(1)
+      ]),
+      cantidadHabitaciones : new FormControl('',[
+        Validators.required,
+        Validators.min(1)
+      ]),
+      telefono : new FormControl('',[
+        Validators.required,
+        Validators.pattern("^((\\+)|(00)|(\\*)|())[0-9]{3,14}((\\#)|())$")
+      ]),
+      web : new FormControl('',[
+        Validators.required,
+        Validators.pattern("(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})")
+      ])
+    });
+
+
+
+    constructor(private _location: Location){}
+
+    ngOnInit() {
+    }
+
+    get nombre(){
+      return this.registrationForm.get('nombre');
+    }
+
+    get telefono(){
+      return this.registrationForm.get('telefono');
+    }
+
+    get capacidad(){
+      return this.registrationForm.get('capacidad');
+    }
+
+    get cantidadHabitaciones(){
+      return this.registrationForm.get('cantidadHabitaciones');
+    }
+
+    get web(){
+      return this.registrationForm.get('web');
+    }
+
+    public goToViewHotels(){
+      this._location.back();
+    }
 
 }
