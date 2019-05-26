a8ce3bcc497249269bcb63051317cfe3b755df2d
Kevin Nava <hombrehealth@hotmail.com>
2019-05-25 21:24:45 -0500

    login with component in falseNgIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from "./app-routing.module";
 
