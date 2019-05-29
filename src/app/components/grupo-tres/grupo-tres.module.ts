322334e39f3fadda0559782e2995553d3a0a048e
Javier Stifano <jstifano18@gmail.com>
2019-05-29 13:50:42 -0400

    Actualizando develop

--- src/app/components/grupo-tres/grupo-tres.module.ts
+++ src/app/components/grupo-tres/grupo-tres.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { NgSelectModule } from '@ng-select/ng-select';
@@ -9,8 +9,6 @@ import { GrupoTresRoutingModule } from './grupo-tres-routing.module';
 import { NewGrupoTres } from '../grupo-tres/pages/new/new.grupo-tres';
 import { ListGrupoTres } from '../grupo-tres/pages/list/list.grupo-tres';
 import { ReactiveFormsModule } from '@angular/forms';
-
-
 
 
 @NgModule({
