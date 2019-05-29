3ec752dcda8341d1ad747d5454cfb798e0550b4b
marcotaku95 <marco4soul@gmail.com>
2019-05-29 09:30:42 -0400

    fix:validaciones en vista reserva de vuelos

--- src/app/components/grupo-doce-vuelos/flight-reservations/flight-reservations.component.ts
+++ src/app/components/grupo-doce-vuelos/flight-reservations/flight-reservations.component.ts
@@ -6,6 +6,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
 import { PeopleFlight } from 'src/app/classes/people_flight';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-flight-reservations',
   templateUrl: './flight-reservations.component.html',
@@ -16,51 +17,38 @@ export class FlightReservationsComponent implements OnInit {
   public form: FormGroup;
   public typeFlights: TypeFlight[]=[];
   public adultFlights:PeopleFlight[]=[];
-  public selectMult:boolean=true;
   public active:boolean=false;
-  public selectMultSearch:number=2;
   public cont:number=3;
   public arrayNumber: number[] = [1,2];
   public disabled:boolean=true;
   public disabledOut:boolean=false;
   public isChecked;
   public subM:boolean=false;
-  public success:boolean=false;
-  constructor(private api: ApiService, private router: Router) { }
-  
+    constructor(private api: ApiService, private router: Router) { }
+        
    private selectedTyp: string="";
    
   ngOnInit() {
-      
       this.typeFlight();
       this.adultFlight();
-      
       this.form=new FormGroup({
-       // classFlight: new FormControl(-1, [Validators.required]),
-        //name: new FormControl(null, [Validators.required]),
         adultFlights: new FormControl(-1,Validators.required),
         origen: new FormControl(null, [Validators.required]),
         destino: new FormControl(null, [Validators.required]),
         fechaS:new FormControl(null, [Validators.required]),
         fechaE:new FormControl(null, [Validators.required])
       })
-    
-
-  }
-
-
-  private typeFlight(): TypeFlight[]{
-    this.typeFlights=[
-      new TypeFlight(0,'Ida'),
-      new TypeFlight(1,'Ida y Vuelta ')
-    ];
-    return this.typeFlights; 
-  }
+     }
+     private typeFlight(): TypeFlight[]{
+        this.typeFlights=[
+        new TypeFlight(0,'Ida'),
+        new TypeFlight(1,'Ida y Vuelta ')
+       ];
+        return this.typeFlights; 
+     }
 
  
   private adultFlight():PeopleFlight[]{
-
-   
       this.adultFlights=[
         new PeopleFlight(1,'a'),
         new PeopleFlight(2,'a'),
@@ -83,7 +71,6 @@ export class FlightReservationsComponent implements OnInit {
     //var element =<HTMLInputElement> document.getElementById("date_id");
     
     if (this.selectedTyp=="0") {
-      this.selectMult=true;
       if (!this.isChecked) {
         input.setAttribute("disabled","true");
       output.removeAttribute("disabled");
@@ -93,42 +80,20 @@ export class FlightReservationsComponent implements OnInit {
       console.log("Estoy en 0");
       }else{
       if(this.selectedTyp=="2"){
-        this.selectMult=false;
         this.arrayNumber;
         this.disabled=false;
        }else{
-           this.selectMult=true;
            if (!this.isChecked && this.disabled==true) {
            input.removeAttribute("disabled");
            output.removeAttribute("disabled");
            this.disabled=false;
           }
-
-           console.log("Estoy en 1");
-        // this.selectMult=true;
+       console.log("Estoy en 1");
       }
       
     }
   }
-
-  clickPushMultidestino(event: any){
-    
-     this.arrayNumber.push(this.cont++);
-     this.active=true;
-    
-  }
-
-  clickPopMultidestino(event: any){
-
-     this.arrayNumber.pop();
-     this.cont--;
-     if(this.cont==3){
-       this.active=false;
-     }
-    
-  }
-
-  checkboxSelected(event: any){
+    checkboxSelected(event: any){
     var element =<HTMLInputElement> document.getElementById("date_id");
 
      this.isChecked= element.checked;
@@ -154,20 +119,21 @@ export class FlightReservationsComponent implements OnInit {
 
   }
   onSubmit() {
-   
-    if (this.form.get('origen').valid && this.form.get('destino').valid
+     if (this.form.get('origen').valid && this.form.get('destino').valid
      && (this.form.get('adultFlights').value !=-1) && (this.form.get('fechaE').valid || this.disabled==true)
      && (this.form.get('fechaS').valid || this.disabledOut==true)) {
       console.log('form submitted');
-      this.success=true;
+
       this.router.navigate(['flight-reservations/list-reservations']);
     } else {
       this.subM=true;
       console.log(this.form.get('adultFlights').value);
-
       console.log('Llenar los campos');    }
   }
   
- 
+  public  postResFlight(){
+     this.api.postUrl('','ssssssssssssssss');
+
+  }
 
 }
