15969182ffad9ba0289457c1e5be2fbaddb01759
Gabriel Tovar <gbrieltovar12@gmail.com>
2019-05-29 18:35:07 -0400

    Congifured Landing route module

--- src/app/components/grupo-uno/grupo-uno-routing.module.ts
+++ src/app/components/grupo-uno/grupo-uno-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GrupoUnoComponent } from './grupo-uno.component';
+import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
   {
       path: '',
       component: GrupoUnoComponent
-  }
+  },
+  {
+     path: 'landing',
+     component: LandingComponent
+  },
+
 ];
 
 @NgModule({
--- src/app/components/grupo-uno/landing/landing-routing.module.ts
+++ src/app/components/grupo-uno/landing/landing-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { LandingComponent } from './landing.component';
+import { Routes, RouterModule, Router } from '@angular/router';
+
+const routes: Routes = [{
+  path: 'landing',
+  component: LandingComponent
+}];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class LandingRoutingModule { }
