26569cbf6a961d2b42cd85301214c123d0ed8f48
brstor <boristor22@gmail.com>
2019-06-01 01:32:29 -0400

    Estructura para mostrar las reservas de restaurant

--- src/app/components/travel/detail-travel/detail-travel.component.ts
+++ src/app/components/travel/detail-travel/detail-travel.component.ts
@@ -8,12 +8,57 @@ import { NgbTabChangeEvent } from '@ng-bootstrap/ng-bootstrap';
 })
 export class DetailTravelComponent implements OnInit {
 
+  restReservations: Array<object>
+
   constructor() { }
 
   ngOnInit() {
   }
 
   onTabChange(event: NgbTabChangeEvent) {
-    console.log(event.nextId)
+    switch (event.nextId) {
+      case 'fly':
+        break;
+      case 'hotel':
+        break;
+      case 'vehicle':
+        break;
+      case 'restaurant':
+        this.getResRest();
+        break;
+    }
+  }
+
+  getResRest() {
+    this.restReservations = [
+      {
+        id: 1,
+        date: '2019-07-20',
+        capacity: 3,
+        restaurant:
+        {
+          id: 22,
+          businessname: 'El rey de las carnes',
+          address_specs: 'Villa de cura via Bella Vista ',
+          specialty: 'Carnes',
+          tlf: '+582443541268',
+          price: 200,
+        }
+      },
+      {
+        id: 2,
+        date: '2019-07-21',
+        capacity: 2,
+        restaurant:
+        {
+          id: 42,
+          businessname: 'Pollo los llanos',
+          address_specs: 'Intercomunal de Cagua',
+          specialty: 'Pollos',
+          tlf: '+582445124365',
+          price: 150,
+        }
+      },
+    ]
   }
 }
