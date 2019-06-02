12c005a205dc09d7d91e08d05f655675eae4e2d9
Juan Goncalves <juan.goncalves498@gmail.com>
2019-06-02 12:49:03 -0400

    feat: cargar pais y ciudad del hotel al mostrar formulario de update

--- src/app/components/view-hotels-backoffice/edit-hotel/edit-hotel.component.ts
+++ src/app/components/view-hotels-backoffice/edit-hotel/edit-hotel.component.ts
@@ -1,15 +1,20 @@
 import { ApiService } from '../../../services/api.service';
 import { Component, OnInit } from '@angular/core';
 import { Role } from '../../../classes/role';
-import { Location } from "@angular/common";
+import { Location } from '@angular/common';
 import Swal from 'sweetalert2';
-import { FormGroup, FormControl, Validators, FormsModule } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormsModule
+} from '@angular/forms';
 import { LocalStorageService } from '../../../services/local-storage.service';
 import { Hotel } from '../../../interfaces/hotel';
 import { transformImageToBase64 } from '../../../utils/global_functions';
 import { environment as url } from '../../../../environments/environment';
 import { SweetAlertOptions } from 'sweetalert2';
-
+import { HotelsService } from '../services/hotels.service';
 
 @Component({
   selector: 'app-edit-hotel',
@@ -18,7 +23,6 @@ import { SweetAlertOptions } from 'sweetalert2';
   providers: [ApiService]
 })
 export class EditHotelComponent implements OnInit {
-
   public hotel: Hotel;
   public urlImage: string;
   public registrationForm: FormGroup;
@@ -27,205 +31,214 @@ export class EditHotelComponent implements OnInit {
   public countries: any[];
   public cities: any[];
 
+  public selectedACountry: boolean;
 
-  constructor(private _location: Location,
-              private service: ApiService,
-              private localStorage: LocalStorageService){
-                this.isDataLoaded = false;
-                this.transformImageToBase64 = transformImageToBase64;
-                this.urlImage = null;
-                this.getCountry();
-              }
+  constructor(
+    private _location: Location,
+    private service: ApiService,
+    private localStorage: LocalStorageService
+  ) {
+    this.isDataLoaded = false;
+    this.transformImageToBase64 = transformImageToBase64;
+    this.urlImage = null;
+  }
 
-
-  ngOnInit(){
+  ngOnInit() {
     this.localStorage.getItem('hotel').subscribe(storedHotel => {
       if (storedHotel) {
         this.isDataLoaded = true;
         this.hotel = storedHotel;
         this.createNewFormGroup(storedHotel);
+        this.getCountry();
       }
     });
   }
-
 
   ngOnDestroy() {
     this.localStorage.removeItem('hotel');
   }
 
-
-  private createNewFormGroup(storedHotel: Hotel){
-    if(storedHotel){
+  private createNewFormGroup(storedHotel: Hotel) {
+    if (storedHotel) {
       this.urlImage = storedHotel['picture'];
       this.registrationForm = new FormGroup({
-          name : new FormControl(storedHotel['name'],[
-            Validators.required,
-            Validators.minLength(5),
-            Validators.maxLength(100)
-          ]),
-          amountOfRooms : new FormControl(storedHotel['amountOfRooms'],[
-            Validators.required,
-            Validators.min(1)
-          ]),
-          roomCapacity : new FormControl(storedHotel['roomCapacity'],[
-            Validators.required,
-            Validators.min(1)
-          ]),
-          addressSpecification: new FormControl(storedHotel['addressSpecification'],[
-            Validators.maxLength(50)
-          ]),
-          pricePerRoom: new FormControl(storedHotel['pricePerRoom'],[
-            Validators.pattern("^[0-9]+(.[0-9]{0,5})?$"),
-            Validators.required,
-            Validators.min(1)
-          ]),
-          phone : new FormControl(storedHotel['phone'],[
-            Validators.pattern("^((\\+)|(00)|(\\*)|())[0-9]{3,14}((\\#)|())$")
-          ]),
-          website : new FormControl(storedHotel['website'],[
-            Validators.pattern("(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})")
-          ]),
-          stars: new FormControl(storedHotel['stars'],[
-            Validators.required,
-            Validators.min(1),
-            Validators.max(5)
-          ]),
-          image: new FormControl(this.urlImage, [
-            Validators.required
-          ]),
-          country: new FormControl(storedHotel['location']['country'], [
-          ]),
-          city: new FormControl(storedHotel['location']['city'], [
-          ])
-        });
-      }
-    }
-
-
-    get name(){
-      return this.registrationForm.get('name');
-    }
-
-    get amountOfRooms(){
-      return this.registrationForm.get('amountOfRooms');
-    }
-
-    get roomCapacity(){
-      return this.registrationForm.get('roomCapacity');
-    }
-
-    get addressSpecification(){
-      return this.registrationForm.get('addressSpecification');
-    }
-
-    get pricePerRoom(){
-      return this.registrationForm.get('pricePerRoom');
-    }
-
-    get phone(){
-      return this.registrationForm.get('phone');
-    }
-
-    get website(){
-      return this.registrationForm.get('website');
-    }
-
-    get stars(){
-      return this.registrationForm.get('stars');
-    }
-
-
-    public getImage(event){
-      this.transformImageToBase64(event, image => {
-        this.urlImage = image
+        name: new FormControl(storedHotel['name'], [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(100)
+        ]),
+        amountOfRooms: new FormControl(storedHotel['amountOfRooms'], [
+          Validators.required,
+          Validators.min(1)
+        ]),
+        roomCapacity: new FormControl(storedHotel['roomCapacity'], [
+          Validators.required,
+          Validators.min(1)
+        ]),
+        addressSpecification: new FormControl(
+          storedHotel['addressSpecification'],
+          [Validators.maxLength(50)]
+        ),
+        pricePerRoom: new FormControl(storedHotel['pricePerRoom'], [
+          Validators.pattern('^[0-9]+(.[0-9]{0,5})?$'),
+          Validators.required,
+          Validators.min(1)
+        ]),
+        phone: new FormControl(storedHotel['phone'], [
+          Validators.pattern('^((\\+)|(00)|(\\*)|())[0-9]{3,14}((\\#)|())$')
+        ]),
+        website: new FormControl(storedHotel['website'], [
+          Validators.pattern(
+            '(https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,})'
+          )
+        ]),
+        stars: new FormControl(storedHotel['stars'], [
+          Validators.required,
+          Validators.min(1),
+          Validators.max(5)
+        ]),
+        image: new FormControl(this.urlImage, [Validators.required]),
+        country: new FormControl(storedHotel['location']['country'], []),
+        city: new FormControl(storedHotel['location']['city'], [])
       });
     }
+  }
 
+  get name() {
+    return this.registrationForm.get('name');
+  }
 
-    public getCountry() {
-      this.service
-          .getUrl(url.endpoint.default._get.getCountry)
-          .then(response => {
-              console.log(response),
-              this.countries = response
-      }, error => console.error(error));
-    }
+  get amountOfRooms() {
+    return this.registrationForm.get('amountOfRooms');
+  }
 
-    public getCity(id: number) {
-      this.service
-          .getUrl(url.endpoint.default._get.getCity, [id.toString()])
-          .then(response => {
-              this.cities = response;
-      }, error => console.error(error));
-    }
+  get roomCapacity() {
+    return this.registrationForm.get('roomCapacity');
+  }
 
-    public selectCountry(event) {
-      this.getCity(event.target.value);
-    }
+  get addressSpecification() {
+    return this.registrationForm.get('addressSpecification');
+  }
 
-    public goToViewHotels(){
-      this._location.back();
-    }
+  get pricePerRoom() {
+    return this.registrationForm.get('pricePerRoom');
+  }
 
+  get phone() {
+    return this.registrationForm.get('phone');
+  }
 
-    public onSubmit(){
-      let idHotel;
-      this.localStorage.getItem('hotel').subscribe( storedHotel => {
-      idHotel = storedHotel['id'];
-      this.service
-      .putUrl(url.endpoint.default._put.putHotel,
-        {
-          id: idHotel,
-          name: this.registrationForm.get('name').value,
-          amountOfRooms: this.registrationForm.get('amountOfRooms').value,
-          roomCapacity: this.registrationForm.get('roomCapacity').value,
-          isActive : true,
-          addressSpecification: this.registrationForm.get('addressSpecification').value,
-          pricePerRoom: this.registrationForm.get('pricePerRoom').value,
-          phone: this.registrationForm.get('phone').value,
-          website: this.registrationForm.get('website').value,
-          picture: this.urlImage,
-          stars: this.registrationForm.get('stars').value,
-          location: {
-            "id" : this.registrationForm.get('city').value
+  get website() {
+    return this.registrationForm.get('website');
+  }
+
+  get stars() {
+    return this.registrationForm.get('stars');
+  }
+
+  public getImage(event) {
+    this.transformImageToBase64(event, image => {
+      this.urlImage = image;
+    });
+  }
+
+  public getCountry() {
+    this.service.getUrl(url.endpoint.default._get.getCountry).then(
+      response => {
+        console.log(response), (this.countries = response);
+        this.countries.forEach(location => {
+          if (location.country == this.hotel.location['country']) {
+            this.registrationForm.get('country').setValue(location.id);
+            this.getCity(location.id);
           }
-        }, [idHotel.toString()])
+        });
+      },
+      error => console.error(error)
+    );
+  }
+
+  public getCity(id: number) {
+    this.service
+      .getUrl(url.endpoint.default._get.getCity, [id.toString()])
       .then(
         response => {
-          this.hotelCreatedSuccessfully(),
-          console.log(response)
-        }).catch(
-          error => {
-            console.log("Hay un error");
-            this.errorOcurred();
-          }
-        );
-     });
-   }
+          this.cities = response;
+          this.cities.forEach(location => {
+            if (location.city == this.hotel.location['city']) {
+              this.registrationForm.get('city').setValue(location.id);
+            }
+          });
+        },
+        error => console.error(error)
+      );
+  }
 
+  public selectCountry(event) {
+    this.getCity(event.target.value);
+  }
 
-     private errorOcurred(){
-       let config: SweetAlertOptions = {
-         title: 'Ha ocurrido un error al agregar el hotel',
-         type: 'error',
-         showConfirmButton: true
-       }
-       Swal.fire(config).then( result =>{
-         //console.log(result);
-       });
-     }
+  public goToViewHotels() {
+    this._location.back();
+  }
 
-     private hotelCreatedSuccessfully(){
-       let config: SweetAlertOptions = {
-         title: 'Hotel Modificado',
-         type: 'success',
-         showConfirmButton: false,
-         timer: 1500
-       }
-       Swal.fire(config).then( result =>{
-         //console.log(result);
-         this.goToViewHotels();
-       });
-     }
+  public onSubmit() {
+    let idHotel;
+    this.localStorage.getItem('hotel').subscribe(storedHotel => {
+      idHotel = storedHotel['id'];
+      this.service
+        .putUrl(
+          url.endpoint.default._put.putHotel,
+          {
+            id: idHotel,
+            name: this.registrationForm.get('name').value,
+            amountOfRooms: this.registrationForm.get('amountOfRooms').value,
+            roomCapacity: this.registrationForm.get('roomCapacity').value,
+            isActive: true,
+            addressSpecification: this.registrationForm.get(
+              'addressSpecification'
+            ).value,
+            pricePerRoom: this.registrationForm.get('pricePerRoom').value,
+            phone: this.registrationForm.get('phone').value,
+            website: this.registrationForm.get('website').value,
+            picture: this.urlImage,
+            stars: this.registrationForm.get('stars').value,
+            location: {
+              id: this.registrationForm.get('city').value
+            }
+          },
+          [idHotel.toString()]
+        )
+        .then(response => {
+          this.hotelCreatedSuccessfully(), console.log(response);
+        })
+        .catch(error => {
+          console.log('Hay un error');
+          this.errorOcurred();
+        });
+    });
+  }
 
+  private errorOcurred() {
+    let config: SweetAlertOptions = {
+      title: 'Ha ocurrido un error al agregar el hotel',
+      type: 'error',
+      showConfirmButton: true
+    };
+    Swal.fire(config).then(result => {
+      //console.log(result);
+    });
+  }
+
+  private hotelCreatedSuccessfully() {
+    let config: SweetAlertOptions = {
+      title: 'Hotel Modificado',
+      type: 'success',
+      showConfirmButton: false,
+      timer: 1500
+    };
+    Swal.fire(config).then(result => {
+      //console.log(result);
+      this.goToViewHotels();
+    });
+  }
 }
