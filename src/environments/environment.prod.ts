243cd5c3062258bfd6d8a4ef1d6f7fae47f3d2aa
Javier Stifano <jstifano18@gmail.com>
2019-06-03 14:36:57 -0400

    Agregando enpoints para el build de production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
+import * as api_endpoints from './api_endpoints.json';
 export const environment = {
   production: true,
   baseApiUrl: "https://localhost:5001/api/",
+  endpoint: api_endpoints
 };
