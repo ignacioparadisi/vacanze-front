796bd9d559ab29eabb35f853448dfa5eca67ff11
Aaron Pereira <aaron.24kilates@hotmail.com>
2019-06-23 15:27:37 -0400

    Get marcas a traves del servicio

--- src/app/components/grupo-cinco/brand/brand.component.ts
+++ src/app/components/grupo-cinco/brand/brand.component.ts
@@ -1,33 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../services/api.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-brand',
   templateUrl: './brand.component.html',
-  styleUrls: ['./brand.component.scss']
+  styleUrls: ['./brand.component.scss'],
+  providers: [ApiService]
 })
 export class BrandComponent {
 
-  constructor() { 
+  constructor(private apiService: ApiService) { 
   	this.getBrands();
   }
 
   brands: Array<object>
 
   getBrands(){
-  	this.brands = [
-  		{
-  			id: 1,
-  			name: "Toyota"
-  		},
-  		{
-  			id: 2,
-  			name: "Chevrolet"
-  		},
-  		{
-  			id: 3,
-  			name: "Honda"
-  		},
-  	]
+  	this.apiService.getUrl('brands').then(
+      (resp) => {
+        this.brands = resp
+       },
+      (fail) => {
+        if (fail.error) {
+          Swal.fire({
+            title: fail.error,
+            type: 'error',
+          })
+        } else {
+          Swal.fire({
+            title: 'Error: ' + fail.status,
+            text: fail.name + '. ' + fail.statusText,
+            type: 'error',
+          })
+        }
+      }
+    );
   }
 
 }
