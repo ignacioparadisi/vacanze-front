c4980017f368b76f52be8a4e27842373b58a6b3c
Aaron Pereira <aaron.24kilates@hotmail.com>
2019-06-23 14:13:34 -0400

    Estructura de la tabla de vehiculos

--- src/app/components/grupo-cinco/fleet/fleet.component.ts
+++ src/app/components/grupo-cinco/fleet/fleet.component.ts
@@ -7,9 +7,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FleetComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    this.getVehicles();
+  }
 
   ngOnInit() {
+  	
+  }
+
+  vehicles: Array<object>
+
+  getVehicles() {
+    this.vehicles = [
+      {
+        id: 1,
+        license: '1A5S4D1',
+        price: 13000,
+        status: true,
+        location:
+        {
+          id: 35,
+          city: 'Caracas'
+        },
+        model:
+        {
+        	id: 1,
+        	capacity: 5,
+        	name: 'Meru',
+        	photo: '',
+        	brand:
+        	{
+        		id: 1,
+        		name: 'Toyota'
+        	}
+        }
+      },
+      {
+        id: 2,
+        license: '45SDAW2',
+        price: 565,
+        status: false,
+        location:
+        {
+          id: 35,
+          city: 'Caracas'
+        },
+        model:
+        {
+        	id: 1,
+        	capacity: 4,
+        	name: 'Corolla',
+        	photo: '',
+        	brand:
+        	{
+        		id: 1,
+        		name: 'Toyota'
+        	}
+        }
+      },
+    ]
+  }
+
+  changeStatus(vehicleId: number){
+    console.log(vehicleId);
   }
 
 }
