7f937896dd15c8331cdd053e9cf251668f2ba577
brstor <boristor22@gmail.com>
2019-06-24 20:05:13 -0400

    Modal para añadir marca

--- src/app/components/grupo-cinco/grupo-cinco.module.ts
+++ src/app/components/grupo-cinco/grupo-cinco.module.ts
@@ -1,28 +1,31 @@
 import { NgModule } from '@angular/core';
-import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { GrupoCincoRoutingModule } from './grupo-cinco-routing.module';
-import { Consulta_autoComponent } from '../grupo-cinco/consultar_auto/consultar_auto';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
-import { Registrar_AutoComponent} from '../grupo-cinco/registrar_auto/registrar_auto'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrandComponent } from './brand/brand.component';
 import { ModelComponent } from './model/model.component';
 import { FleetComponent } from './fleet/fleet.component';
+import { CreateBrandComponent } from './brand/create-brand/create-brand';
 
 @NgModule({
-    declarations: [Consulta_autoComponent,Registrar_AutoComponent, BrandComponent, ModelComponent, FleetComponent],
-    imports: [
-      CommonModule,
-      GrupoCincoRoutingModule,
-      HttpClientModule,
-      NgbModule,
-      FormsModule,
-      SweetAlert2Module.forRoot(),
-      ReactiveFormsModule
-    ],
-    providers: []
-  })
-  export class GrupoCincoModule {}
-  
+  declarations: [
+    BrandComponent,
+    ModelComponent,
+    FleetComponent,
+    CreateBrandComponent
+  ],
+  imports: [
+    CommonModule,
+    GrupoCincoRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    SweetAlert2Module.forRoot(),
+    ReactiveFormsModule,
+    NgbModalModule
+  ],
+  providers: []
+})
+export class GrupoCincoModule { }
