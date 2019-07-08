f2f57398a05a13f8098c453076781cf130fb0d04
rafael sanchez <rafaelsanchez814@gmail.com>
2019-07-08 02:25:29 -0400

    PostBaggageCheck

--- src/app/components/grupo-once-checkin/grupo-once-checkin.component.ts
+++ src/app/components/grupo-once-checkin/grupo-once-checkin.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import Swal, { SweetAlertOptions } from 'sweetalert2';
+import { ApiService } from '../../services/api.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { environment as url } from '../../../environments/environment';
 @Component({
   selector: 'app-grupo-once-checkin',
   templateUrl: './grupo-once-checkin.component.html',
@@ -9,13 +12,17 @@ import Swal, { SweetAlertOptions } from 'sweetalert2';
 export class GrupoOnceCheckinComponent implements OnInit {
 
   private bagList: Array<any> = [];
+  private PostBagList: Array<any> = [];
   private bagdet: any = {};
-
+  public ticket: any;
+  constructor( private serv: ApiService) {}
   initAddBag()
   {
     this.bagdet.reference="";
     this.bagdet.weigth= "";
     this.bagdet.descrip= "";
+    this.bagdet.fli= "";
+    this.bagdet.cru= 0;
   }
   private messageError(message: string) {
     let config: SweetAlertOptions = {
@@ -57,8 +64,49 @@ export class GrupoOnceCheckinComponent implements OnInit {
   private getValuereference(event) {this.bagdet.reference= event.target.value;}
   private getValueweigth(event) {this.bagdet.weigth= event.target.value;}
   private getValuedescrip(event) {this.bagdet.descrip= event.target.value;}
+  private getTicketId(event) {this.bagdet.fli= event.target.value;}
 
-  constructor() {}
+  public Registrar()
+  {
+    if(this.bagdet.fli="")
+    {
+      this.messageError("Debe completar los campos")
+    }
+    else
+    {
+      this.serv.postUrl(url.endpoint.default._post.Baggage, this.bagList)
+      .then(response => {
+        this.alertSucces(response);
+      })
+      .catch((err: HttpErrorResponse) => {
+        this.alertErrror(err.error)
+      })
+    }
+  }
+
+  private alertSucces(message: string) {
+    let config: SweetAlertOptions = {
+      title: message,
+      type: 'success',
+      showConfirmButton: true,
+      timer: 5000
+    }
+    Swal.fire(config).then(result => {
+    });
+  }
+
+  private alertErrror(error: string) {
+    let config: SweetAlertOptions = {
+      title: error,
+      type: 'error',
+      showConfirmButton: false,
+      timer: 3000
+    }
+    Swal.fire(config).then(result => {
+    });
+  }
+
+
   ngOnInit() 
   {
     this.initAddBag();
