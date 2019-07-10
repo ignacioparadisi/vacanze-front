a8d72593aaabefd3fff3e1ed40a05c47b570d6b8
brstor <boristor22@gmail.com>
2019-07-10 18:00:26 -0400

    Modificar vehiculo funcional

--- src/app/components/grupo-cinco/fleet/modify-vehicle/modify-vehicle.ts
+++ src/app/components/grupo-cinco/fleet/modify-vehicle/modify-vehicle.ts
@@ -16,13 +16,14 @@ import { Veh_Model } from '../../../../classes/veh_model';
 export class ModifyVehicleComponent {
 
     @Output() spread = new EventEmitter();
+    @Input('idV') idV: number;
     @Input('modelId') modelId: number;
     @Input('brandId') brandId: number;
-    @Input('countryId') countryId: number;
     @Input('cityId') cityId: number;
     @Input('licenseV') licenseV: string;
     @Input('priceV') priceV: number;
     @Input('statusV') statusV: number;
+    @Input('vehicles') vehicles: Array<Object>;
     activeModal: NgbModalRef;
     formGroup: FormGroup;
     vehicleForm: FormGroup;
@@ -37,11 +38,14 @@ export class ModifyVehicleComponent {
     }
 
     open(content) {
+        this.getBrands();
+        this.getModels(this.brandId);
+        this.getCountries();
         this.perolForm = new FormGroup({
-            countryId: new FormControl(this.countryId),
             brandId: new FormControl(this.brandId)
         });
         this.vehicleForm = new FormGroup({
+            id: new FormControl(this.idV, Validators.required),
             vehicleModelId: new FormControl(this.modelId, Validators.required),
             vehicleLocationId: new FormControl(this.cityId, Validators.required),
             license: new FormControl(this.licenseV, [Validators.required, Validators.maxLength(10)]),
@@ -49,9 +53,6 @@ export class ModifyVehicleComponent {
             status: new FormControl(this.statusV)
         });
         this.selectedCountry = false;
-        this.getBrands();
-        this.getModels(this.brandId);
-        this.getCountries();
         this.activeModal = this.modalService.open(content);
     }
 
@@ -149,24 +150,50 @@ export class ModifyVehicleComponent {
     }
 
     modifyVehicle() {
-        this.apiService.postUrl('vehicles', this.vehicleForm.value).then(
-            (resp) => {
-                this.closeModal();
-                Swal.fire({
-                    title: '!Éxito¡',
-                    text: 'El vehículo se creó satisfactoriamente.',
-                    type: 'success'
-                });
-                this.spread.emit();
-            },
-            (fail) => {
-                Swal.fire({
-                    title: 'Codigo: ' + fail.status,
-                    text: fail.error,
-                    type: 'error',
-                })
-            }
-        );
+        if (!this.vehicles.find(x => this.licenseV == this.vehicleForm.get("license").value))
+            this.apiService.putUrl('vehicles', this.vehicleForm.value).then(
+                (resp) => {
+                    this.closeModal();
+                    Swal.fire({
+                        title: '!Éxito¡',
+                        text: 'El vehículo se modificó satisfactoriamente.',
+                        type: 'success'
+                    });
+                    this.spread.emit();
+                },
+                (fail) => {
+                    Swal.fire({
+                        title: 'Codigo: ' + fail.status,
+                        text: fail.error,
+                        type: 'error',
+                    })
+                }
+            );
+        if (this.vehicles.find(x => this.licenseV == this.vehicleForm.get("license").value) && this.vehicleForm.dirty)
+            this.apiService.putUrl('vehicles', this.vehicleForm.value).then(
+                (resp) => {
+                    this.closeModal();
+                    Swal.fire({
+                        title: '!Éxito¡',
+                        text: 'El vehículo se modificó satisfactoriamente.',
+                        type: 'success'
+                    });
+                    this.spread.emit();
+                },
+                (fail) => {
+                    Swal.fire({
+                        title: 'Codigo: ' + fail.status,
+                        text: fail.error,
+                        type: 'error',
+                    })
+                }
+            );
+        else
+            Swal.fire({
+                title: 'Codigo: 405',
+                html: 'El vehículo <b>' + this.licenseV + '</b> con esas especificaciones ya existe.',
+                type: 'error',
+            })
     }
 
 }
